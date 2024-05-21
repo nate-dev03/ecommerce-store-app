a0d97cbfe5512101d31c771ce3f8566dae0e2ed8
nate-dev03 <leinsomar19@gmail.com>
2024-05-21 19:38:25 +0800

    Added connection to a internal postgressql from render.com dashboard

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,7 +1,21 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import { Client } from 'pg';
 
 const app = express();
+
+const client = new Client({
+  user: "products_zcbp_user",
+  host: "dpg-cp6l2pev3ddc73fm873g-a",
+  database: "postgres://products_zcbp_user:VGm8UmhXttHj4yEjLt4QZiiT0O7pKaeX@dpg-cp6l2pev3ddc73fm873g-a/products_zcbp",
+  password: "VGm8UmhXttHj4yEjLt4QZiiT0O7pKaeX",
+  port: 5432,
+});
+
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected");
+})
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
